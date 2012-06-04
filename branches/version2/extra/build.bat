a660292d42de389e391f03ab043e154cea2281b9
Temujin2887 <Temujin2887@9267c565-2304-6706-dcb0-5975f87540a4>
2012-06-04 22:45:00 +0000

    Clearing out old files

--- branches/version2/extra/build.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd ..
-C:\python27\python.exe .\extra\build_spec.py build --build-exe .\extra\build
-pause
--- branches/version2/run.bat
+++
@@ -1,2 +0,0 @@
-Z:\tools\binnt\python\python26_x86\python.exe .\main.pyw
-pause
